ec31f01d62592a9c1b780c046be4ec47df937ea3
Alphonso Santos <alphonsoesantos@gmail.com>
2023-04-24 00:45:01 +0800

    progress app.core

---
+++ app/core/config.py
@@ -0,0 +1,26 @@
+import pytz
+import os
+import sys
+
+from typing import Any, Dict, List, Optional, Union
+from pydantic import BaseSettings
+from dotenv import load_dotenv
+
+# NOTE: __file__ = /gcp-fastapi-backend/app/core/config.py, os.path.dirname(os.path.dirname(os.path.dirname())) = /gcp-fastapi-backend
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+load_dotenv(os.path.join(BASE_DIR, '.env'))
+
+class Settings(BaseSettings):
+	POSTGRES_USER: str = os.environ.get('POSTGRES_USER')
+	POSTGRES_PASSWORD: str = os.environ.get('POSTGRES_PASSWORD')
+	POSTGRES_SERVER: str = os.environ.get('POSTGRES_SERVER')
+	POSTGRES_PORT: str = os.environ.get('POSTGRES_PORT')
+	POSTGRES_DB: str = os.environ.get('POSTGRES_DB')
+	DATABASE_URL: str = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}/{POSTGRES_DB}"
+	TIMEZONE: Any = pytz.timezone('Asia/Singapore')
+	
+	class Config:
+		case_sensitive = True
+
+settings = Settings()
--- app/main.py
+++ app/main.py
@@ -1,60 +1,21 @@
-import uvicorn
-from fastapi import FastAPI
-from fastapi_sqlalchemy import DBSessionMiddleware, db
-
-from app.schemas import Book as SchemaBook
-from app.schemas import Author as SchemaAuthor
-
-from app.schemas import Book
-from app.schemas import Author
-
-from app.models import Book as ModelBook
-from app.models import Author as ModelAuthor
-
-import os
-from dotenv import load_dotenv
-
-load_dotenv('.env')
-
+from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
+# from app.api.api_v1.api import api_router
+from app.core.config import settings
 
 app = FastAPI()
-# uvicorn app.main:app --reload
-
-# to avoid csrftokenError
-app.add_middleware(DBSessionMiddleware, db_url=os.environ['DATABASE_URL'])
 
 @app.get("/")
 async def root():
     return {"message": "hello world"}
 
+# app.add_middleware(
+# 	CORSMiddleware,
+# 	allow_origins = ['*'],
+# 	allow_credentials = True,
+# 	allow_methods = ['*'],
+# 	allow_headers = ['*']
+# )
 
-@app.post('/book/', response_model=SchemaBook)
-async def book(book: SchemaBook):
-    db_book = ModelBook(title=book.title, rating=book.rating, author_id = book.author_id)
-    db.session.add(db_book)
-    db.session.commit()
-    return db_book
-
-@app.get('/book/')
-async def book():
-    book = db.session.query(ModelBook).all()
-    return book
-
-
-  
-@app.post('/author/', response_model=SchemaAuthor)
-async def author(author:SchemaAuthor):
-    db_author = ModelAuthor(name=author.name, age=author.age)
-    db.session.add(db_author)
-    db.session.commit()
-    return db_author
-
-@app.get('/author/')
-async def author():
-    author = db.session.query(ModelAuthor).all()
-    return author
-
-
-# To run locally
-if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=8000)
+# app.include_router(api_router, prefix = settings.API_V1_STR)
+# uvicorn app.main:app --reload
